c71bbe1222ec2d10dde95dc3cf89eb5fcd5e335d
hpaulicin <henocpaulicinjr@gmail.com>
2018-03-05 21:20:18 -0500

    Login Test script
    this is my first code :-( just trying to put together what i've learned

---
+++ Login test script.py
@@ -0,0 +1,12 @@
+import webbrowser
+import ctypes
+while (True):
+   username = input("Please Enter Username: ")
+   password = input("Please Enter your password: ")
+
+   if (username == "jonesc16@gmail.com" and password == "Wenoc2017"):
+      webbrowser.open("https://www.facebook.com/christine.jones.31586")
+   else:
+      print("your login is incorrect")
+   
+    
